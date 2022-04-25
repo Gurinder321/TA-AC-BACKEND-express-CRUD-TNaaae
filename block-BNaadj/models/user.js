0b70f6dc29d450d22625a160cef5e287c456d6a4
Gurinder Khabra <gurinderkhabra@gmail.com>
2022-04-25 15:22:32 -0700

    done

---
+++ block-BNaadj/models/user.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true },
+    age: Number,
+    address: String,
+    bio: String,
+    hobbies: String,
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
